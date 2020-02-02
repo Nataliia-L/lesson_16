34c06dc5bb0559a49b2002510fa7c63e97aa11d4
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2020-02-02 15:58:06 +0200

    Create hw01.py

---
+++ hw01.py
@@ -0,0 +1,30 @@
+# Попросите пользователя ввести имя файла. 
+# Поддерживаемые расширения - txt, log, py. 
+# Если введено неподдерживаемое расширение, кидать исключение. 
+# Если введенный файл не существует, кидать исключение. Подсчитать количество слов в файле.
+
+import os
+def verify_file ():
+    global file_name
+    file_name = input ('Please, enter name: ')
+    if file_name.endswith ('.txt'):
+        print ('Great! The file_name is: ', file_name)
+    elif file_name.endswith ('.log'):
+        print ('Great! The file_name is: ', file_name)
+    elif file_name.endswith ('.py'):
+        print ('Great! The file_name is: ', file_name)
+    else:
+        raise Exception ('Sorry! The name is incorect.')
+    
+    ex = os.path.exists (file_name)
+    if ex==True:
+        print ('Congrats! Your file exists!')
+    else:
+        raise Exception ('Sorry! Your file does not exist!')
+    
+def calc_words ():
+    verify_file ()
+    print ('The number of words in the file name: ', len (file_name.split('.')))
+
+
+calc_words ()
